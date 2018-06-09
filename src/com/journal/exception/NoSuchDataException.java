b67c54b7acf6ce68d40543704b93d58791aa6ff4
Iryna Pfeiffer <iryna.pfeiffer@gmail.com>
2018-06-09 11:33:33 +0300

    Edit validator

--- src/com/journal/exception/NoSuchDataException.java
+++ src/com/journal/exception/NoSuchDataException.java
@@ -4,11 +4,13 @@ public class NoSuchDataException extends Exception {
     private String message;
 
     public NoSuchDataException(String message) {
+
         this.message = message;
     }
 
     @Override
     public String getMessage() {
+
         return message;
     }
 }
--- src/com/journal/validator/Validador.java
+++ src/com/journal/validator/Validador.java
@@ -10,47 +10,40 @@ public class Validador {
     private static Pattern regexPattern;
     private static Matcher regMatcher;
 
-    public static boolean validateEmailAddress(String emailAddress) {
+    public static void validateEmailAddress(String emailAddress) throws IncorrectInputException {
 
         regexPattern = Pattern.compile("^[(a-zA-Z-0-9-\\_\\+\\.)]+@[(a-z-A-z)]+\\.[(a-zA-z)]{2,3}$");
-        regMatcher   = regexPattern.matcher(emailAddress);
-        if(regMatcher.matches()) {
-            return true;
-        } else {
-            System.out.println("Incorrectly input email");
-            return false;
-        }
-    }
 
-    public static boolean validateMobileNumber(String mobileNumber) {
+            if (!regexPattern.matcher(emailAddress).matches()) {
+                throw new IncorrectInputException("Incorrectly input e-mail address");
+            }
+        }
+
+    public static void validateMobileNumber(String mobileNumber) throws IncorrectInputException {
         regexPattern = Pattern.compile("^\\+[0-9]{2,3}+-[0-9]{10}$");
-        regMatcher   = regexPattern.matcher(mobileNumber);
-        if(regMatcher.matches()) {
-            return true;
-        } else {
-            System.out.println("Incorrectly input mobile number");
-            return false;
+
+        if (!regexPattern.matcher(mobileNumber).matches()) {
+         throw new IncorrectInputException("Incorrectly input number");
         }
     }
 
     public static void validateGroup(String group) throws IncorrectInputException {
         regexPattern = Pattern.compile("[a-zA-Z-а-яА-Я]{1,3}[0-9]{1,2}");
-        regMatcher   = regexPattern.matcher(group);
-        if(!regexPattern.matcher(group).matches()) {
+        if (!regexPattern.matcher(group).matches()) {
             throw new IncorrectInputException("Incorrectly input group");
         }
     }
 
     public static void validateText(String str) throws IncorrectInputException {
         regexPattern = Pattern.compile("[A-ZА-ЯІ][a-z-а-яії]{1,15}",Pattern.UNICODE_CHARACTER_CLASS);
-        if(!regexPattern.matcher(str).matches()){
+        if (!regexPattern.matcher(str).matches()){
             throw new IncorrectInputException("Incorrectly input text");
         }
     }
 
     public static void validateNumber(String str) throws IncorrectInputException {
         regexPattern = Pattern.compile("[1-9][0-9]{0,15}",Pattern.UNICODE_CHARACTER_CLASS);
-        if(!regexPattern.matcher(str).matches()) {
+        if (!regexPattern.matcher(str).matches()) {
             throw new IncorrectInputException("Incorrectly input number");
         }
     }
