22e657a4e3737d7383cce471df06875cda5f8ee4
Volodymyr <vlad5855051@gmail.com>
2018-06-09 12:47:04 +0300

    add model group

---
+++ src/com/journal/model/Group.java
@@ -0,0 +1,31 @@
+package com.journal.model;
+
+public class Group {
+    private int id;
+    private String nameGroup;
+
+    public Group(Builder builder) {
+        this.nameGroup = builder.nameGroup;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getGroup() {
+        return nameGroup;
+    }
+
+    public static class Builder {
+        private String nameGroup;
+
+        public Builder setGroup(String nameGroup) {
+            this.nameGroup = nameGroup;
+            return this;
+        }
+
+        public Group build() {
+            return new Group(this);
+        }
+    }
+}
