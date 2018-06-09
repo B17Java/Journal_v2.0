24b2b08757f1a684a9226ccdca2938cb51186556
Oleksandr Kuzmenko <okuzmenkoa@gmail.com>
2018-06-09 13:18:32 +0300

    edit Student

--- src/com/journal/model/Student.java
+++ src/com/journal/model/Student.java
@@ -57,7 +57,40 @@ public class Student {
         private String phone;
         private String nikGit;
 
+        public Builder setFirstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
 
+        public Builder setLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder setAge(String age) {
+            this.age = age;
+            return this;
+        }
+
+        public Builder setAddress(String address) {
+            this.address = address;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setPhone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public Builder setNikGit(String nikGit) {
+            this.nikGit = nikGit;
+            return this;
+        }
 
         public Student build() {
             return new Student(this);
